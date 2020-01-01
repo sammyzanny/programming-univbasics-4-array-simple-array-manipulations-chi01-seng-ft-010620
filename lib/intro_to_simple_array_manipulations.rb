807bb87169baa64480f559313d39715ba77d9c3d
Samuel Zandi <samuelczandi@gmail.com>
2020-01-01 19:14:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,7 +30,21 @@ def using_insert(array,element)
   array.insert(element,4)
 end  
   
+  def using_uniq
+    
+  end
   
+  def using_flatten
+    
+  end
+  
+  def using_delete
+    
+  end
+  
+  def using_delete_at
+    
+  end
   
   
   
